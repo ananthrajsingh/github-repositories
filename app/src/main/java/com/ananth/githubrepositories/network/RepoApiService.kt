7c45af5b9641eb66edc40d461179df71753bc27e
ananthrajsingh <ananthrajsingh@gmail.com>
2020-06-13 00:34:06 +0530

    Add RepoApiService. This will be used to fetch the repositories data over network

---
+++ app/src/main/java/com/ananth/githubrepositories/network/RepoApiService.kt
@@ -0,0 +1,46 @@
+package com.ananth.githubrepositories.network
+
+import com.ananth.githubrepositories.model.Repository
+import com.ananth.githubrepositories.util.Constant
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+/**
+ * Build the Moshi object that Retrofit will be using, adding the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Retrofit object to assist in talking to the server.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(Constant.BASE_URL)
+    .build()
+
+/**
+ * Public interface to expose the [getRepositories] method
+ */
+interface RepoApiService {
+    /**
+     * Returns a Coroutine [Deferred] [List] of [Repository] which can be fetched with await() if
+     * in a Coroutine scope.
+     */
+    @GET(Constant.REPOSITORIES_ENDPOINT)
+    fun getRepositories(): Deferred<List<Repository>>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object RepoApi {
+    val retrofitService : RepoApiService by lazy { retrofit.create(RepoApiService::class.java) }
+}
---
+++ app/src/main/java/com/ananth/githubrepositories/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.ananth.githubrepositories.util
+
+/**
+ * Holds compile-time constants used throughout the project.
+ */
+object Constant {
+    /* Network Constants */
+    const val BASE_URL = "api.github.com"
+    const val REPOSITORIES_ENDPOINT = "repositories"
+}
