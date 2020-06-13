3f6954e58e87ab5b5936eb4c0633055e5816eb8b
ananthrajsingh <ananthrajsingh@gmail.com>
2020-06-13 21:01:20 +0530

    Implement Glide to show images

--- app/src/main/java/com/ananth/githubrepositories/util/BindingAdapters.kt
+++ app/src/main/java/com/ananth/githubrepositories/util/BindingAdapters.kt
@@ -1,11 +1,15 @@
 package com.ananth.githubrepositories.util
 
 import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.ananth.githubrepositories.R
 import com.ananth.githubrepositories.list.RepoListAdapter
 import com.ananth.githubrepositories.model.Repository
-
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 
 @BindingAdapter("listData")
@@ -23,5 +27,22 @@ fun View.showOnlyWhenEmpty(data: List<Repository>?) {
     visibility = when {
         data == null || data.isEmpty() -> View.VISIBLE
         else -> View.GONE
+    }
+}
+
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
     }
 }
